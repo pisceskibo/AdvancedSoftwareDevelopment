d15023a0ba62c78e0c74abd0f5d3adde2f457452
pisceskibo <taquangtung2003@gmail.com>
2025-12-30 15:05:48 +0700

    finish exam03

--- Week02_TaQuangTung_25007892/Exam03.py
+++ Week02_TaQuangTung_25007892/Exam03.py
@@ -7,6 +7,23 @@ Nếu có nhiều đoạn con đối xứng dài nhất, in ra đoạn xuất hi
 VD: babad => bab
 """
 
+def is_palindrome(s):
+    return s == s[::-1]
+
 def find_substring_palindrome(string_input):
-    pass
-strng_input = input("Enter string: ")
+    n = len(string_input)
+    longest = ""
+
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            substring = string_input[i:j]
+
+            if is_palindrome(substring):
+                if len(substring) > len(longest):
+                    longest = substring
+
+    return longest
+
+string_input = input("Enter string: ")
+keyword_palindrome = find_substring_palindrome(string_input)
+print(f"Substring of Palidrome is: {keyword_palindrome}")
